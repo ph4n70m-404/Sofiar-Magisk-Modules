e4597726e24d1cfb362d7f0a289245732ae026e9
ph4n70m-404 <87523412+ph4n70m-404@users.noreply.github.com>
2021-07-26 10:01:14 -0500

    Add files via upload

--- common/install.sh
+++ common/install.sh
@@ -3,16 +3,28 @@ grep -q "ro.product.system.device=sofiar" /system/build.prop
 if [[ $? = 0 ]]; then
   grep -q "ro.build.flavor=evolution_sofiar-userdebug" /system/build.prop
   if [[ $? = 0 ]]; then
-    echo "Installing for Evolution X"
+    echo "Installing for Evolution X 5.9"
     cp -R $MODPATH/temp/evolution/system $MODPATH
   else
     grep -q "ro.build.flavor=xtended_sofiar-eng" /system/build.prop
     if [[ $? = 0 ]]; then
-      echo "Installing for MSM Xtended XR"
+      echo "Installing for MSM Xtended XR 7.0"
       cp -R $MODPATH/temp/xtended/system $MODPATH
     else
-      echo "You are using an unsupported rom"
-      exit 1
+      grep -q "ro.build.flavor=bliss_sofiar-userdebug" /system/build.prop
+      if [[ $? = 0 ]]; then
+        echo "Installing for Bliss 14.5"
+        cp -R $MODPATH/temp/bliss/system $MODPATH
+      else
+        grep -q "ro.build.flavor=spark_sofiar-userdebug" /system/build.prop
+        if [[ $? = 0 ]]; then
+          echo "Installing for Spark vFlare"
+          cp -R $MODPATH/temp/spark/system $MODPATH
+        else
+          echo "You are using an unsupported rom"
+          exit 1
+        fi
+      fi
     fi
   fi
 else
